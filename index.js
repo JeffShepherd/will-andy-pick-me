36877cf0d14feba379497534c781a17ffa01925e
Jeff S <jeff.shep777@gmail.com>
2021-01-04 11:23:45 -0700

    Finish iteration 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//
 var formInput = document.querySelector('.form-input');
 var getAnswerButton = document.querySelector('.get-answer-button');
 var clearButton = document.querySelector('.clear-button')
@@ -13,17 +13,21 @@ clearButton.addEventListener('click', resetLeftContainer)
 
 
 function displayAnswer(event) {
-event.preventDefault();
-question.innerText = `"${formInput.value}"`;
-answer.innerText = getRandomAnswer();
-eightballImage.classList.add('hidden');
-results.classList.remove('hidden');
-clearButton.classList.add('clear-button-active');
+  event.preventDefault();
+  if (formInput.value != "") {
+    question.innerText = `"${formInput.value}"`;
+    answer.innerText = getRandomAnswer();
+    eightballImage.classList.add('hidden');
+    results.classList.remove('hidden');
+    clearButton.classList.add('clear-button-active');
+    formInput.value = "";
+  } else {
+    alert('Please ask a question');
+  }
 }
 
 function resetLeftContainer() {
   clearButton.classList.remove('clear-button-active');
-  formInput.value = "";
   eightballImage.classList.remove('hidden');
   results.classList.add('hidden');
 }
@@ -36,24 +40,24 @@ function getRandomAnswer() {
 
 
 var answers = [
-'It is certain.',
-'It is decidedly so.',
-'Without a doubt.',
-'Yes - definitely.',
-'You may rely on it.',
-'As I see it, yes.',
-'Most likely.',
-'Outlook good.',
-'Yes.',
-'Signs point to yes.',
-'Reply hazy, try again.',
-'Ask again later.',
-'Better not tell you now.',
-'Cannot predict now.',
-'Concentrate and ask again.',
-'Don\'t count on it.',
-'My reply is no.',
-'My sources say no.',
-'Outlook not so good.',
-'Very doubtful.'
+  'It is certain.',
+  'It is decidedly so.',
+  'Without a doubt.',
+  'Yes - definitely.',
+  'You may rely on it.',
+  'As I see it, yes.',
+  'Most likely.',
+  'Outlook good.',
+  'Yes.',
+  'Signs point to yes.',
+  'Reply hazy, try again.',
+  'Ask again later.',
+  'Better not tell you now.',
+  'Cannot predict now.',
+  'Concentrate and ask again.',
+  'Don\'t count on it.',
+  'My reply is no.',
+  'My sources say no.',
+  'Outlook not so good.',
+  'Very doubtful.'
 ]
